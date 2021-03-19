aae68e5547e1867a5cd82d7369480961321c4978
Atessa Amjadi <aamjadi@wisc.edu>
2021-03-19 14:34:44 -0500

    scrollview and welcome label

--- MarketMania/Main/HomeVC.swift
+++ MarketMania/Main/HomeVC.swift
@@ -17,12 +17,10 @@ class HomeVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.addSubview(collectionView)
-        
         setUpViews()
         
-        collectionView.delegate = self
-        collectionView.dataSource = self
+        collectionView1.delegate = self
+        collectionView1.dataSource = self
         
         
     }
@@ -35,12 +33,36 @@ class HomeVC: UIViewController {
     // MARK: UI Setup
     //
     
+    let scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        //sv.backgroundColor = .white
+        return sv
+    }()
     
+    let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.add(text: "Welcome back...", font: UIFont.boldSystemFont(ofSize: 25.0), textColor: .black)
+        label.textAlignment = .center
+        return label
+        
+    }()
     
-    let collectionView: UICollectionView = {
+    let collectionView1: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        return cv
+    }()
+    
+    let collectionView2: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        //layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .white
         cv.translatesAutoresizingMaskIntoConstraints = false
         cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
         return cv
@@ -58,17 +80,23 @@ class HomeVC: UIViewController {
     func setUpViews() {
         //self.view.backgroundColor = .red
         
-        view.addSubviews(views: [collectionView, topMovesLabel])
+        view.addSubview(scrollView)
+
+        scrollView.addSubviews(views: [welcomeLabel, collectionView1, topMovesLabel])
+        
+        scrollView.anchor(view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
        
-        collectionView.backgroundColor = .white
+        
         
         
         //make the collection view only the height of one cell
-        collectionView.heightAnchor.constraint(equalTo: collectionView.widthAnchor, multiplier: 0.4).isActive = true
+        collectionView1.heightAnchor.constraint(equalTo: collectionView1.widthAnchor, multiplier: 0.4).isActive = true
         
-        collectionView.anchor(view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, topConstant: view.frame.height/2.5, leftConstant: 20, bottomConstant: 0, rightConstant: 20, widthConstant: 0, heightConstant: 0)
+        collectionView1.anchor(view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, topConstant: view.frame.height/2.5, leftConstant: 20, bottomConstant: 0, rightConstant: 20, widthConstant: 0, heightConstant: 0)
 
-        topMovesLabel.anchor(nil, left: view.leftAnchor, bottom: collectionView.topAnchor, right: nil, topConstant: 0, leftConstant: 20, bottomConstant: 10, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        topMovesLabel.anchor(nil, left: view.leftAnchor, bottom: collectionView1.topAnchor, right: nil, topConstant: 0, leftConstant: 20, bottomConstant: 10, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        welcomeLabel.anchor(view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, topConstant: 20, leftConstant: 20, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
         
        
     }
