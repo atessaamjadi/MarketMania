476ad3fe7916b3926f11602bf5060e2e09b90202
Atessa Amjadi <aamjadi@wisc.edu>
2021-03-24 22:29:53 -0500

    HomeVC now is all customizable cells for scrolling

---
+++ MarketMania/Main/HomeVC/WatchlistView.swift
@@ -0,0 +1,104 @@
+//
+//  WatchlistView.swift
+//  MarketMania
+//
+//  Created by Atessa Amjadi on 3/24/21.
+//
+
+import UIKit
+
+class WatchlistView: UICollectionViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+        
+        collectionView2.delegate = self
+        collectionView2.dataSource = self
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    let watchListLabel: UILabel = {
+        let label = UILabel()
+        label.add(text: "Watchlist", font: UIFont(name: "PingFangHK-Regular", size: 15)!, textColor: .gray)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    //watchlist collection view
+    let collectionView2: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = .menu_white
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        
+        // register cells
+        cv.register(watchListCell.self, forCellWithReuseIdentifier: "watchListCell")
+        return cv
+    }()
+    
+    func setUpViews() {
+        
+        contentView.addSubviews(views: [watchListLabel, collectionView2])
+        
+        
+        watchListLabel.anchor(contentView.topAnchor, left: contentView.leftAnchor, bottom: collectionView2.topAnchor, right: nil, topConstant: 20, leftConstant: 10, bottomConstant: 5, rightConstant: 20, widthConstant: 0, heightConstant: 0)
+
+        collectionView2.anchor(watchListLabel.bottomAnchor, left: contentView.leftAnchor, bottom: contentView.safeAreaLayoutGuide.bottomAnchor, right: contentView.rightAnchor, topConstant: 0, leftConstant: 10, bottomConstant: 0, rightConstant: 10, widthConstant: 0, heightConstant: 0)
+             
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 21
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "watchListCell", for: indexPath) as! watchListCell
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height/5)
+    }
+    
+}
+
+//cells for watchlist
+class watchListCell: UICollectionViewCell {
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .systemTeal
+        
+        setUpViews()
+    }
+    
+    let tempLabel: UILabel = {
+        let label = UILabel()
+        label.add(text: "testing", font: UIFont(name: "PingFangHK-Regular", size: 15)!, textColor: .black)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    func setUpViews() {
+        
+//        contentView.addSubview(tempLabel)
+        
+//        tempLabel.anchor(contentView.topAnchor, left: contentView.leftAnchor, bottom: nil, right: nil, topConstant: 5, leftConstant: 5, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+      
+       
+       
+    }
+}
---
+++ MarketMania/Main/HomeVC/WelcomeView.swift
@@ -0,0 +1,50 @@
+//
+//  WelcomeView.swift
+//  MarketMania
+//
+//  Created by Atessa Amjadi on 3/24/21.
+//
+
+import UIKit
+
+
+class WelcomeView: UICollectionViewCell {
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .menu_white
+        
+        setUpViews()
+    }
+    
+    let welcomeLabel: UILabel = {
+        let label = UILabel()
+        label.add(text: "Welcome back...", font: UIFont.boldSystemFont(ofSize: 25.0), textColor: .darkGray)
+        label.textAlignment = .center
+        return label
+        
+    }()
+    
+    let tempSarcasticLabel: UILabel = {
+        let label = UILabel()
+        label.add(text: "Ready to lose more $ ?", font: UIFont(name: "PingFangHK-Regular", size: 15)!, textColor: .gray)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    func setUpViews() {
+        
+        contentView.addSubviews(views: [welcomeLabel, tempSarcasticLabel])
+        
+        welcomeLabel.anchor(contentView.safeAreaLayoutGuide.topAnchor, left: contentView.leftAnchor, bottom: nil, right: nil, topConstant: 30, leftConstant: 10, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+        tempSarcasticLabel.anchor(welcomeLabel.bottomAnchor, left: contentView.leftAnchor, bottom: nil, right: nil, topConstant: 10, leftConstant: 20, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+       
+    }
+    
+}
